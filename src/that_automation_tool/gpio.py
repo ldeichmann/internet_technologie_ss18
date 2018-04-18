720b924a076d418d4575585bec61b389c4314eff
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2018-04-18 19:54:38 +0200

    Stupid gpio example

--- src/that_automation_tool/gpio.py
+++ src/that_automation_tool/gpio.py
@@ -1,4 +1,26 @@
 import RPi.GPIO as gpio
+gpio.setmode(gpio.BOARD)
+
+led_channel_no = 40
+led2_channel_no = 38
+ldr_channel_no = 36
+
+ldr_channel = gpio.setup(ldr_channel_no, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+led_channel = gpio.setup(led_channel_no, gpio.OUT)
+led2_channel = gpio.setup(led2_channel_no, gpio.OUT)
+
+
+def ldr_high():
+    return gpio.input(ldr_channel_no)
+
+
+def led_on():
+    gpio.output(led_channel_no, 1)
+    gpio.output(led2_channel_no, 0)
+
+def led_off():
+    gpio.output(led_channel_no, 0)
+    gpio.output(led2_channel_no, 1)
 
 
 # do stuff here
--- src/that_automation_tool/main.py
+++ src/that_automation_tool/main.py
@@ -1,5 +1,19 @@
-from .communication import Communication
+import time
 
+from .communication import Communication
+from .gpio import ldr_high, led_on, led_off
 
 if __name__ == "__main__":
-    print("This is being executed")
+    print("This is being executed on the raspi")
+    i = 0
+    while True:
+        time.sleep(1)
+        if ldr_high():
+            print(i, "LDR is high")
+            led_on()
+            # led_off()
+        else:
+            print(i, "LDR is low")
+            # led_on()
+            led_off()
+        i += 1
