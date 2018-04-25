dcf51f566bf20db8bf3b83f71991868c406072de
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2018-04-25 12:46:20 +0200

    Change gpio to event driven usage

--- src/that_automation_tool/gpio.py
+++ src/that_automation_tool/gpio.py
@@ -1,26 +1,42 @@
-import RPi.GPIO as gpio
-gpio.setmode(gpio.BOARD)
-
-led_channel_no = 40
-led2_channel_no = 38
-ldr_channel_no = 36
-
-ldr_channel = gpio.setup(ldr_channel_no, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-led_channel = gpio.setup(led_channel_no, gpio.OUT)
-led2_channel = gpio.setup(led2_channel_no, gpio.OUT)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
 
 
-def ldr_high():
-    return gpio.input(ldr_channel_no)
+class _GPIOCallback:
+    """
+    Container to allow returning actual values to the callback func
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, channel):
+        value = GPIO.input(channel)
+        self.func(channel, value)
 
 
-def led_on():
-    gpio.output(led_channel_no, 1)
-    gpio.output(led2_channel_no, 0)
+class GPIOHandler:
 
-def led_off():
-    gpio.output(led_channel_no, 0)
-    gpio.output(led2_channel_no, 1)
+    @staticmethod
+    def enable_callback(channel):
+        GPIO.add_event_detect(channel, GPIO.BOTH)
 
+    @staticmethod
+    def register_callback(channel, callback):
+        GPIO.add_event_callback(channel, _GPIOCallback(callback))
 
-# do stuff here
+    @staticmethod
+    def set_input(channel):
+        GPIO.setup(channel, GPIO.IN)
+
+    @staticmethod
+    def set_output(channel):
+        GPIO.setup(channel, GPIO.OUT)
+
+    @staticmethod
+    def turn_on(channel):
+        GPIO.output(channel, 1)
+
+    @staticmethod
+    def turn_off(channel):
+        GPIO.output(channel, 0)
--- src/that_automation_tool/main.py
+++ src/that_automation_tool/main.py
@@ -1,19 +1,43 @@
 import time
+import platform
 
 from .communication import Communication
-from .gpio import ldr_high, led_on, led_off
+from .gpio import GPIOHandler
+
+
+led_channel_no = 40
+led2_channel_no = 38
+ldr_channel_no = 37
+
 
 if __name__ == "__main__":
-    print("This is being executed on the raspi")
-    i = 0
-    while True:
-        time.sleep(1)
-        if ldr_high():
-            print(i, "LDR is high")
-            led_on()
-            # led_off()
+    if platform.machine() == "armv8":
+        print("This is being executed on the raspi")
+    else:
+        print("Probably not running on the raspi")
+    gpio_handler = GPIOHandler()
+    gpio_handler.set_output(led_channel_no)
+    gpio_handler.set_output(led2_channel_no)
+    gpio_handler.set_input(ldr_channel_no)
+
+
+    def ldr_on_cb():
+        gpio_handler.turn_on(led_channel_no)
+        gpio_handler.turn_off(led2_channel_no)
+
+    def ldr_off_cb():
+        gpio_handler.turn_off(led_channel_no)
+        gpio_handler.turn_on(led2_channel_no)
+
+    def ldr_cb(channel, value):
+        if value:
+            ldr_on_cb()
         else:
-            print(i, "LDR is low")
-            # led_on()
-            led_off()
-        i += 1
+            ldr_off_cb()
+
+    gpio_handler.enable_callback(ldr_channel_no)
+    gpio_handler.register_callback(ldr_channel_no, ldr_cb)
+
+    while True:
+        time.sleep(5)
+        print("Still alive")
